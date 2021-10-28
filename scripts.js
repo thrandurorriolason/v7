820f0bcf0d8a2c932ccf2bede839a4976e63796c
thrandurorriolason <yrandur@gmail.com>
2021-10-28 23:53:10 +0000

    loka

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ let losses = 0;
  * @return {boolean} true eða false
  */
 function isValidBestOf(bestOf) {
-  if (bestOf % 2 === 0) {
+  if (bestOf % 2 === 0 && bestOf < MAX_BEST_OF) {
     return false;
   }
   else{
@@ -30,7 +30,6 @@ console.assert(isValidBestOf(2) === false, '2 er ekki er valid best of');
 console.assert(isValidBestOf(9) === true, '9 er valid best of');
 
 function playAsText(play) {
-  
   if (play === "1"){
     return "Skæri";
   }
@@ -38,10 +37,10 @@ function playAsText(play) {
     return "Blað";
   }
   else if (play === "3"){
-    return 'Steinn';
+    return "Steinn";
   }
   else {
-    return console.error("Óþekkt");
+    return "Óþekkt";
   }
 }
 console.assert(playAsText('1') === 'Skæri', '1 táknar skæri');
@@ -56,20 +55,20 @@ console.assert(playAsText('foo') === 'Óþekkt', 'Annað er óþekkt');
  * @returns -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function checkGame(player, computer) {
-  if (player == 1 && computer == 2){
-    return "Skæri vinnur blað";
+  if (player == 1 && computer == 1){
+    return 0;
   }
-  else if (player == 2 && computer == 3){
-    return "Blað vinnur stein";
-  }
-  else if (player == 3 && computer == 1){
-    return "Steinn vinnur skæri";
-  }
-  else if (player == 1 && computer == 1){
-    return "Skæri og skæri eru jafntefli";
+  else if (player == 1 && computer == 2){
+    return 1;
   }
   else if (player == 1 && computer == 3){
-    return "Skæri tapar fyrir stein";
+    return -1;
+  }
+  else if (player == 2 && computer == 3){
+    return 1;
+  }
+  else if (player == 3 && computer == 1){
+    return 1;
   }
 }
 console.assert(checkGame('1', '2') === 1, 'Skæri vinnur blað');
@@ -83,41 +82,28 @@ console.assert(checkGame('1', '3') === -1, 'Skæri tapar fyrir stein');
  * @return {boolean} -1 ef tölva vann, 0 ef jafntefli, 1 ef spilari vann
  */
 function round() {
-  // TODO útfæra
-  // 1. Spyrja um hvað spilað, ef cancel, hætta
-  // 2. Ef ógilt, tölva vinnur
-  // 3. Velja gildi fyrir tölvu með `Math.floor(Math.random() * 3) + 1` sem skilar heiltölu á [1, 3]
-  // 4. Nota `checkGame()` til að finna hver vann
-  // 5. Birta hver vann
-  // 6. Skila hver vann
+  var playerrr = prompt("Hverju viltu spila? Skæri(1), Blað(2), Steinn(3): ")
+  computerrr = Math.floor(Math.random() * 3) + 1
+  return checkGame(playerrr, computerrr);
+  
 }
-// Hér getum við ekki skrifað test þar sem fallið mun biðja notanda um inntak!
 
 /**
  * Spilar leik og bætir útkomu (sigur eða tap) við í viðeigandi global breytu.
  */
 function play() {
-  // TODO útfæra
-  // 1. Spyrja um fjölda leikja
-  // 2. Staðfesta að fjöldi leikja sé gilt gildi
-  // 3. Keyra fjölda leikja og spila umferð þar til sigurvegari er krýndur
-  // 4. Birta hvort spilari eða tölva vann
   bestOf = parseInt(bestOf);
   var bestOf = prompt("Hvað viltu spila marga leiki?: ")
-  isValidBestOf(bestOf);
-  
-  
-  
-  var play = prompt("Hverju viltu spila?: ")
-  complay = 2;
-  checkGame(play, complay);
+  if (isValidBestOf(bestOf) == true) {
+    return round();
+  }
 }
-// Hér getum við ekki skrifað test þar sem fallið mun biðja notanda um inntak!
 
 /**
  * Birtir stöðu spilara.
  */
 function games() {
-  // TODO útfæra
+  console.log("Þú hefur spilað "+Math.abs(wins+losses)+" leiki.");
+  console.log("Þú hefur unnið "+wins+", eða "+(wins)/(Math.abs(wins+losses))+" af heild.");
+  console.log("Þú hefur tapað "+losses+", eða "+(losses)/(Math.abs(wins+losses))+" af heild.");
 }
-// Hér getum við ekki skrifað test þar sem fallið les úr global state
